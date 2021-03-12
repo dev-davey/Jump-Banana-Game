a2aab6e81f4010c9dcfe0d87e15cca314cb2bafb
davey <davey89451@gmail.com>
2021-03-12 13:55:24 -0800

    alert working

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var character = document.getElementById('character');
 var block = document.getElementById('block');
 
-function jump(){
-    if (character.classList != 'animate'){
-    character.classList.add('animate');
+function jump() {
+    if (character.classList != 'animate') {
+        character.classList.add('animate');
     }
-    setTimeout(function(){
-        character.classList.remove('animate')}
-    ,500);
+    setTimeout(function () {
+        character.classList.remove('animate')
+    }
+        , 500);
 }
 
 var checkDead = setInterval(function() {
@@ -15,11 +16,6 @@ var checkDead = setInterval(function() {
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<70 && blockLeft>30 && characterTop>=130){
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        alert("Game Over");
     }
 }, 10);
