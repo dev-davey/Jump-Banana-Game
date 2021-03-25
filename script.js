55e011b090b6f2b0a1f09b992982db28f41ec0a7
davey <davey89451@gmail.com>
2021-03-25 14:38:54 -0700

    start btn added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const character = document.getElementById('character');
 const block = document.getElementById('block');
+const gameOver = document.getElementById('game-over');
+const startBtn = document.getElementById('start');
+
+
+
 
 function jump() {
     if (character.classList != 'animate') {
@@ -15,17 +20,27 @@ const checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<70 && blockLeft>30 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over");
+        block.style.animation = "hidden";
+        gameOver.textContent = "Game Over Try Again!"
+        startBtn.style.display = "block";
     }
 }, 10);
 
 let currJump = 0;
 let count = document.getElementById('score');
-count.textContent = 'Score: ' + currJump;
+count.textContent = 'Score: ' + currJump
 addEventListener("click", score);
 
 function score(){
-    currJump++;
-    count.textContent = 'Score:' + currJump;
+        currJump++;
+        count.textContent = 'Score:' + currJump;
+};
+
+document.getElementById('start').addEventListener("click", reStart);
+
+function reStart(){
+    currJump = 0;
+    gameOver.style.display = "none";
+    startBtn.style.display = "none";
+    block.style.animation = "visible";
 }
